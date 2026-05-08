bb42dab282a4f89d79074efc0cb9aba69ecc950b
bienvenudev <cbienvenu007@gmail.com>
2026-05-08 16:35:41 +0200

    feat: use a more structured approach with a data class to store the test data and expected outcomes

--- src/test/java/org/example/UserTest.java
+++ src/test/java/org/example/UserTest.java
@@ -1,40 +1,64 @@
 package org.example;
 
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.*;
+public class UserTest {
 
-class UserTest {
+    static class TestData {
+        String username;
+        String password;
+        boolean expectedValidUsername;
+        boolean expectedStrongPassword;
+        boolean expectedValid;
 
-    User user;
-    boolean expected;
+        public TestData(String username, String password, boolean expectedValidUsername, boolean expectedStrongPassword, boolean expectedValid) {
+            this.username = username;
+            this.password = password;
+            this.expectedValidUsername = expectedValidUsername;
+            this.expectedStrongPassword = expectedStrongPassword;
+            this.expectedValid = expectedValid;
+        }
+    }
 
-    static String[] names;
-    static String[] passwords;
-    static boolean[] expectedOutcomes;
-
+    static TestData[] testData;
     static int index = 0;
 
     @BeforeAll
     static void setUp() {
-        names = new String[] {"Alice", "Alice", "Alice", "", null, "    "};
-        passwords = new String[] {"12345678", "123", null, "12345678", "12345678", "12345678"};
-        expectedOutcomes = new boolean[] {true, false, false, false, false, false};
+        testData = new TestData[] {
+                new TestData("Alice", "12345678", true, true, true),
+                new TestData("Alice", "123", true, false, false),
+                new TestData("Alice", null, true, false, false),
+                new TestData("", "12345678", false, true, false),
+                new TestData(null, "12345678", false, true, false),
+                new TestData("    ", "12345678", false, true, false),
+        };
     }
 
     @BeforeEach
     void createUser() {
-        user = new User(names[index], passwords[index]);
-        expected = expectedOutcomes[index];
+        User user = new User(testData[index].username, testData[index].password);
+        // You might need to set this user to a field if you need to access it in your test methods
     }
 
-    @AfterEach
-    void incrementIndex() {
-        index += 1;
+    @RepeatedTest(value = 6, name = "user test {currentRepetition}/{totalRepetitions}")
+    void testUser() {
+        TestData data = testData[index];
+        User user = new User(data.username, data.password);
+        boolean isValidUsername = user.hasValidUsername();
+        boolean hasStrongPassword = user.hasStrongPassword();
+        boolean isValid = user.isValid();
+
+        assertEquals(data.expectedValidUsername, isValidUsername);
+        assertEquals(data.expectedStrongPassword, hasStrongPassword);
+        assertEquals(data.expectedValid, isValid);
+
+        index++;
     }
 
-    @RepeatedTest(value = 6, name = "user.isValid() test {currentRepetition}/{totalRepetitions}")
-    void isValid() {
-        assertEquals(expected, user.isValid());
+    @AfterAll
+    static void tearDown() {
+        index = 0;
     }
 }
